967c313dd1e96aed75238ac70a8960f37d4fa663
Brian Cunningham <brian.c.cunningham@gmail.com>
2023-01-13 14:45:00 -0500

    reorder routes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './Login';
 import Layout from "./Layout";
-
 import {
     Route,
     Switch,
-    Link
 } from 'react-router-dom';
 import {fakeInterests, fakeSkills} from "./dummy-data";
 
@@ -15,8 +13,8 @@ function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path='/' component={Layout} />
         <Route path='/login' component={Login} />
+        <Route path='/' component={Layout} />
       </Switch>
     </div>
   );
