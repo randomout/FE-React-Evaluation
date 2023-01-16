546b1bd245b0e28c505fe8b28b9f488a18ea7cad
Brian Cunningham <brian.c.cunningham@gmail.com>
2023-01-15 20:13:38 -0500

    update home page

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { loadInterests, loadSkills } from './actions'
 
+import './Home.css';
+
 function Home() {
   const user = useSelector((state) => state.user);
   const interests = useSelector((state) => state.user.interests);
@@ -16,7 +18,15 @@ function Home() {
   
   return (
     <div className="Home">
-      Welcome {user.userName}
+      <p className="welcome">Welcome {user.userName}</p>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod 
+      bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra 
+      justo commodo. Proin sodales pulvinar sic tempor. Sociis natoque penatibus 
+      et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla 
+      luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus pronin sapien 
+      nunc accuan eget.
+      </p>
     </div>
   )
 }
