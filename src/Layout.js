e0014ada0e2fd3e058ceae44f75915726f2dc4a3
Brian Cunningham <brian.c.cunningham@gmail.com>
2023-01-15 19:39:42 -0500

    add navbar styling (not yet complete)

--- src/Layout.js
+++ src/Layout.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { Route, Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import Home from './Home';
 import Interests from './Interests';
 import Skills from './Skills';
+
+import './Layout.css';
 
 function Layout() {
   const user = useSelector((state) => state.user);
 
   return (
     <div className="Layout">
-       <nav>
+       <nav className="navbar">
+          <img className="logo" src="logo.png" />
           <ul>
             <li>
               <Link to="/">Home</Link>
@@ -22,8 +26,9 @@ function Layout() {
             <li>
               <Link to="/skills">Skills</Link>
             </li>
-            <li>
-              {user.userName}
+            <li className="user">
+              <FontAwesomeIcon icon="fa-solid fa-user" />   
+              <span>{user.userName}</span>
             </li>
           </ul>
         </nav>
